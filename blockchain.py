4a10dc9062ea742b11f1d3adbbe29daf6e514868
qqiumax <maxqqqq@hotmail.com>
2023-07-09 17:23:19 +0800

    Made code more clean, renamed file

--- blockchain.py
+++ blockchain.py
@@ -18,6 +18,20 @@ class Block(object):
         return sha512(block_string.encode()).hexdigest()
 
 
+def proof_of_work(block):
+    block.nonce = 0
+    computed_hash = block.hash()
+    while not computed_hash.startswith('0' * Blockchain.difficulty):
+        block.nonce += 1
+        computed_hash = block.hash()
+    return computed_hash
+
+
+def is_valid_proof(block, block_hash):
+    return (block_hash.startswith('0' * Blockchain.difficulty) and
+            block_hash == block.hash())
+
+
 class Blockchain(object):
     def __init__(self):
         self.unconfirmed_transactions = []
@@ -35,37 +49,21 @@ class Blockchain(object):
 
     difficulty = 2
 
-
-    def proof_of_work(self,block):
-        block.nonce = 0
-        computed_hash = block.hash()
-        while not computed_hash.startswith('0' * Blockchain.difficulty):
-            block.nonce += 1
-            computed_hash = block.hash()
-        return computed_hash
-
-
     def add_block(self, block, proof):
         previous_hash = self.last_block.hash
         if previous_hash != block.previous_hash:
             return False
-        if not self.is_valid_proof(block, proof):
+        if not is_valid_proof(block, proof):
             return False
         block.hash = proof
         self.chain.append(block)
         return True
 
-
-    def is_valid_proof(self,block, block_hash):
-        return (block_hash.startswith('0' * Blockchain.difficulty) and
-                block_hash == block.hash())
-
-
     def add_new_transaction(self, transaction):
         self.unconfirmed_transactions.append(transaction)
 
-    def send(self,sender,recipient,amount):
-        nb = (sender,recipient,amount)
+    def send(self, sender, recipient, amount):
+        nb = (sender, recipient, amount)
         self.add_new_transaction(nb)
 
     def mine(self):
@@ -79,7 +77,7 @@ class Blockchain(object):
                           timestamp=time.time(),
                           previous_hash=last_block.hash)
 
-        proof = self.proof_of_work(new_block)
+        proof = proof_of_work(new_block)
         self.add_block(new_block, proof)
         self.unconfirmed_transactions = []
         return new_block.index
@@ -89,8 +87,8 @@ app = Flask(__name__)
 
 blockchain = Blockchain()
 
-blockchain.send("Max","Henry",10)
-blockchain.mine()
+
+
 @app.route('/', methods=['GET'])
 def get_chain():
     chain_data = []
@@ -104,8 +102,6 @@ def get_chain():
     o.close()
 
     return oe
-
-
 
 
 app.run(debug=False, port=8000)
