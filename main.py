f6ccf91518ae1206c8a01173ddd030f5b0fa5e52
qqiumax <maxqqqq@hotmail.com>
2023-07-09 13:11:41 +0800

    Stopped problems Ready to publish beta

--- main.py
+++ main.py
@@ -36,57 +36,61 @@ class Blockchain(object):
     difficulty = 2
 
 
-def proof_of_work(block):
-    block.nonce = 0
-    computed_hash = block.hash()
-    while not computed_hash.startswith('0' * Blockchain.difficulty):
-        block.nonce += 1
+    def proof_of_work(self,block):
+        block.nonce = 0
         computed_hash = block.hash()
-    return computed_hash
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.hash()
+        return computed_hash
 
 
-def add_block(self, block, proof):
-    previous_hash = self.last_block.hash
-    if previous_hash != block.previous_hash:
-        return False
-    if not self.is_valid_proof(block, proof):
-        return False
-    block.hash = proof
-    self.chain.append(block)
-    return True
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+        if previous_hash != block.previous_hash:
+            return False
+        if not self.is_valid_proof(block, proof):
+            return False
+        block.hash = proof
+        self.chain.append(block)
+        return True
 
 
-def is_valid_proof(block, block_hash):
-    return (block_hash.startswith('0' * Blockchain.difficulty) and
-            block_hash == block.hash())
+    def is_valid_proof(self,block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.hash())
 
 
-def add_new_transaction(self, transaction):
-    self.unconfirmed_transactions.append(transaction)
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
 
+    def send(self,sender,recipient,amount):
+        nb = (sender,recipient,amount)
+        self.add_new_transaction(nb)
 
-def mine(self):
-    if not self.unconfirmed_transactions:
-        return False
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
 
-    last_block = self.last_block
+        last_block = self.last_block
 
-    new_block = Block(index=last_block.index + 1,
-                      transactions=self.unconfirmed_transactions,
-                      timestamp=time.time(),
-                      previous_hash=last_block.hash)
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
+                          previous_hash=last_block.hash)
 
-    proof = self.proof_of_work(new_block)
-    self.add_block(new_block, proof)
-    self.unconfirmed_transactions = []
-    return new_block.index
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
 
 
 app = Flask(__name__)
 
 blockchain = Blockchain()
 
-
+blockchain.send("Max","Henry",10)
+blockchain.mine()
 @app.route('/', methods=['GET'])
 def get_chain():
     chain_data = []
@@ -102,7 +106,9 @@ def get_chain():
     return oe
 
 
-app.run(debug=True, port=8000)
+
+
+app.run(debug=False, port=8000)
 
 # You can further put the api on any server you want
 # This is much needed as I do not have a server
